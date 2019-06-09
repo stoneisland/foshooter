b36cd2c5a2350319f9768c6228e9f3a6a3e70830
Nicolò Perfetto <gianluca.fioletti@gmail.com>
2019-06-09 16:41:00 +0200

    Add bin property

--- TODO.md
+++ TODO.md
@@ -5,3 +5,7 @@
 * Document methods
 
 * Handle errors
+
+* Debug library
+
+* <https://www.airpair.com/node.js/posts/top-10-mistakes-node-developers-make>
--- src/inputs.ts
+++ src/inputs.ts
@@ -4,7 +4,7 @@ import yargs = require('yargs');
 config({ path: '.env' });
 
 const argv = yargs
-  .usage('Usage: node $0 --in [in] --out [out')
+  .usage('Usage: node $0 --in [in] --out [out]')
   .option('in', {
     alias: 'i',
     description: 'Input Folder',
